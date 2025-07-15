06e187cea3749d5418e782a7b2bdf85a7b050007
dorenwgiskezkn <dorenwgiskezkn@hotmail.com>
2025-07-15 11:37:12 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve static files
+app.use(express.static("public"));
+
+// Catch-all route to serve sub-index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "sub-index.html"));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
