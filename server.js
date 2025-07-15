b4bad9fef99a323af64cb0daf159aff36e9873f0
alan <168492923+alan349@users.noreply.github.com>
2025-07-15 11:57:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files from 'public' folder
+// Serve static files
 app.use(express.static("public"));
 
-// Rewrite all other routes to sub-index.html
+// Catch-all route to serve sub-index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "sub-index.html"));
 });
 
-// Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server is running on port", port);
+  console.log("Server running on port", port);
 });
